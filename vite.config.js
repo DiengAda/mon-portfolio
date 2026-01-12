2e580a62d7427940d93a1ff092eecc241d8381ea
DiengAda <adamadieng1957@gmail.com>
2026-01-12 01:16:06 +0100

    Mise à jour de la configuration Vite pour le nom du dépôt 'mon-portfolio'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/adama-dieng-portfolio/', // Remplacez par le nom de votre repo GitHub
+  base: '/mon-portfolio/', // Remplacez par le nom de votre repo GitHub
 })
